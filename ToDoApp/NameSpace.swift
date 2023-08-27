c0e34548f63e93ecaacb0e98958b909730d5c27d
zaehorang <zaehorang2@naver.com>
2023-08-28 01:13:41 +0900

    add 스토리보드 구성

---
+++ ToDoApp/NameSpace.swift
@@ -0,0 +1,15 @@
+//
+//  NameSpace.swift
+//  ToDoApp
+//
+//  Created by zaehorang on 2023/08/26.
+//
+
+import UIKit
+
+// 사용하게될 Cell 문자열 묶음
+public struct Cell {
+    static let toDoCellIdentifier = "ToDocCell"
+    static let toDoTableViewCellIdentifier = "ToDoTableViewCell"
+    private init() {}
+}

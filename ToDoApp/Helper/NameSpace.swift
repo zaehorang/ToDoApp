a6c5d7e5001123686b6a5fa57f998e05d8b32673
zaehorang <zaehorang2@naver.com>
2023-08-31 16:27:50 +0900

    fix : modelName 수정 & 오타 수정

--- ToDoApp/Helper/NameSpace.swift
+++ ToDoApp/Helper/NameSpace.swift
@@ -9,14 +9,16 @@ import UIKit
 
 // 사용하게될 Cell 문자열 묶음
 public struct Cell {
-    static let toDoCellIdentifier = "ToDocCell"
-    static let toDoTableViewCellIdentifier = "ToDoTableViewCell"
+    static let toDoCellIdentifier = "ToDoCell"
     private init() {}
 }
 
 // 엔터티 이름 (코어데이터에 저장된 객체)
-
 public struct CoreData {
-    static let modelName: String = "MemoData"
+    static let modelName: String = "ToDoData"
+    private init() {}
 }
 
+public struct Segue {
+    static let name: String = "ToDoCell"
+}
